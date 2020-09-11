2046f9e49a69901228aba212aee105c20da7bc1c
Greg Jett <greg.jett25@gmail.com>
2020-09-10 19:18:28 -0500

    add event listener to highlight seats when select

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const container = document.querySelector(".container");
-const seat = document.querySelectorAll(".seat");
+// grabs all the seats in the row that aren't occupied
+const seat = document.querySelectorAll(".row.seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+const ticketPrice = +movieSelect.value;
+
+// event listener
+// toggles seat to highlight when selected
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+  }
+});
