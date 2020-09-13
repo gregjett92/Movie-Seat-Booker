de03de00ccb9909c31879304cff3cb4637347e26
Greg Jett <greg.jett25@gmail.com>
2020-09-13 16:26:15 -0500

    added another event listener (doesn't work yet)

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const container = document.querySelector(".container");
 // grabs all the seats in the row that aren't occupied
-const seat = document.querySelectorAll(".row.seat:not(.occupied)");
+const seats = document.querySelectorAll(".row.seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+
+// Update total count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row.seat.selected");
+
+  // length of the array
+  const selectedSeatsCount = selectedSeats.length;
+
+  // ticket price plus ticket count
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
 
 // event listener
 // toggles seat to highlight when selected
